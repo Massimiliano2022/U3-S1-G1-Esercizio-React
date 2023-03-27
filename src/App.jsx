8cb2bc37aec5212fb46bca62831297ee0863f386
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-03-27 17:19:16 +0200

    esercizio-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/ButtonComponent';
+import ButtonComponent from './components/ButtonComponent';
+import './components/ImgComponent';
+import ImgComponent from './components/ImgComponent';
 
 function App() {
   return (
@@ -17,6 +21,8 @@ function App() {
         >
           Learn React
         </a>
+        <ButtonComponent action="Invia"/>
+        <ImgComponent src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpngGRjYX1ca7qAADU3K6eGLj7ShQE3L2otdzfryl_Y9Ht2QRoQKYQbsXd36XIxMbYOw0" alt="logo"/>
       </header>
     </div>
   );
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,11 @@
+const ButtonComponent = props => {
+
+    return(
+        <>
+        <button>{props.action}</button>
+        </>
+    )
+
+};
+
+export default ButtonComponent;
